93ffe9396adea93e05dad0b567e6855cdaff07bf
Vaibhav-Sandhir <vaibhavsandhir1@gmail.com>
2024-03-28 12:32:09 +0530

    Convolutions

---
+++ Convolutions/Image.h
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <stdint.h>
+
+struct Image {
+    uint8_t* data = NULL;
+    size_t size = 0;
+    int w;
+    int h;
+    int channels;
+
+    Image(const char* filename);
+    Image(int w, int h, int channels);
+    Image(Image& img);
+    ~Image();
+
+    bool read(const char* filename);
+    bool write(const char* filename);
+    Image& std_convolve(uint32_t ker_width, uint32_t ker_height, double ker[], uint32_t centre_r, uint32_t centre_c);
+};
---
+++ Convolutions/main.cpp
@@ -0,0 +1,11 @@
+#include "Image.h"
+
+int main(int argc, char** argv){
+    Image test("b.png");
+    double ker[] = {2/16.0, 4/16.0, 2/16.0,
+                     4/16.0, 8/16.0, 4/16.0,
+                     2/16.0, 4/16.0, 2/16.0};               
+    test.std_convolve(3, 3, ker, 1, 1);               
+    test.write("highcontrast.png");
+    return 0;
+}
